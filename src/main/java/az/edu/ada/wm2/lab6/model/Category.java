57cce6bd40366eeb7c710d126b220db618af6774
Ismayil <150678829+Ismayil338@users.noreply.github.com>
2026-04-02 18:17:48 +0400

    Add CategoryEntity and CategoryRepository

---
+++ src/main/java/az/edu/ada/wm2/lab6/model/Category.java
@@ -0,0 +1,36 @@
+package az.edu.ada.wm2.lab6.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Builder.Default;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Category {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "categories")
+    @Default
+    private Set<Product> products = new HashSet<>();
+}
